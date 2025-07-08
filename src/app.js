69c5f63f58c392f88459257d9c9ee96edb78a58e
MakharashviliBeqa <201153206+MakharashviliBeqa@users.noreply.github.com>
2025-07-08 15:25:03 +0400

    Fixed everything And Done

--- src/app.js
+++ src/app.js
@@ -1,30 +1,33 @@
-const regForm = document.querySelector('#regForm')
-const personalNumber = document.querySelector('#personalNumber')
+const regForm = document.querySelector('#registration-form')
+const personalNumber = document.querySelector('#personal-number')
 const password  = document.querySelector('#password')
 const email = document.querySelector('#email')
-const errorMessage = document.querySelector('#errorMessage')
 
 
 regForm.addEventListener('submit', (e) => {
     e.preventDefault();
-})
 
-const pn = personalNumber.value.trim();
-const isPNValid = /^\d{11}$/.test(pn);
+    const pn = personalNumber.value.trim();
+    const isPNValid = /^\d{11}$/.test(pn);
 
-const em = email.value.trim();
-const isEmailValid = em.endsWith("@gmail.com");
+    const em = email.value.trim();
+    const isEmailValid = em.endsWith("@gmail.com");
 
-const pw = password.value.trim();
-const isPWValid = pw.length >=8;
+    const pw = password.value.trim();
+    const isPWValid = pw.length >=8;
 
 
-if (!isPNValid) {
-    errorMessage.textContent = "Personal number must be 11 digits!";
-} else if (!isEmailValid) {
-    errorMessage.textContent = "Email must end with @gmail.com!";
-} else if (!isPWValid) {
-    errorMessage.textContent = "Password must be at least 8 characters!";
-} else {
-    errorMessage.textContent = "All good!";
-}
+    if (!isPNValid) {
+        console.log("Personal number must be exactly 11 digits!");
+    }
+
+    if (!isEmailValid) {
+        console.log("Email must end with @gmail.com!");
+    }
+
+    if (!isPWValid) {
+        console.log("Password must be at least 8 characters!");
+    } else {
+        console.log("All good!");
+    }
+});
